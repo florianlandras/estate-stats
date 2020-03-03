6b6272e4856a1199b63578b9893ed0bd6ea607c7
Florian Landras <landrasflorian@gmail.com>
2020-03-03 18:34:53 +0100

    v.1

---
+++ postProcess/cent21/getCent21.py
@@ -0,0 +1,40 @@
+#import post process
+
+from urllib import request
+from urllib.parse import urlencode
+from bs4 import BeautifulSoup
+# TODO process multiple pages
+
+
+
+def get (dictionnaire) :
+    """in dictionnaire
+       out url
+    """
+    baseUrl = "https://www.century21.fr/annonces/"
+
+    if dictionnaire["achatmaison"] == "oui" and dictionnaire["achatappart"] == "non" :
+        txtBien = "achat-maison/"
+    if dictionnaire["achatappart"] == "oui" and dictionnaire["achatmaison"] == "non": 
+        txtBien = "achat-appart/"
+    if dictionnaire["achatappart"] == "oui" and dictionnaire["achatmaison"] == "oui":
+        txtBien = "achat-maison-appartement/"
+    
+    ville = "v-" + dictionnaire["ville"]
+    urlCent21 = baseUrl + txtBien + ville
+    return urlCent21
+    
+    
+
+#https://www.century21.fr/annonces/achat-maison-appartement/v-toulon/alentours-15/s-0-/st-0-/b-0-/page-1/
+if __name__ == '__main__':
+    dictionnaire = {
+        "achatmaison":"oui",
+        "achatappart":"non",
+        "ville" : "toulon",
+
+
+    }
+    print(get(dictionnaire))
+    
+    
