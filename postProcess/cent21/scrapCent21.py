161770a7531b0ba23f3d8c42a4ad4f4184ad2668
Florian Landras <landrasflorian@gmail.com>
2020-03-04 12:28:04 +0100

    finish create dataframe

--- postProcess/cent21/scrapCent21.py
+++ postProcess/cent21/scrapCent21.py
@@ -23,13 +23,37 @@ def getDf (url) :
         priceArray.append(temp)
 
 
-    # list of area
+    # list of area + nbr de room
     areaArray = []
+    nbrRoomArray = []
+    area = soup.find_all( "h4" , attrs = { "class" : "detail_vignette"})
+
+    for a in area : 
+        temp2, temp3, temp4 = re.findall(r"[-+]?\d*\.\d+|\d+", a.get_text()) # getting ['240', '000']
+        temp2 = temp2+"."+temp3
+        temp2 = float(temp2)
+        areaArray.append(temp2)
+        nbrRoomArray.append(temp4)
     
+
     #list of price per square meters
     pricePerSquareMeterArray = []
+    for i in range(len(priceArray)):
+        pricePerSquareMeterArray.append(float(priceArray[i]/areaArray[i]))
         
-    print(priceArray)
+    #print(priceArray)
+    #print(areaArray)
+    #print(nbrRoomArray)
+    
+    # Create the pandas DataFrame
+    
+    data = { 
+        "area":areaArray,
+        "nbrRoom": nbrRoomArray,
+        "price": priceArray,
+        "price/m2": pricePerSquareMeterArray, #TODO normalize df columns names
+    }
+    return  pd.DataFrame(data)
 
 
     
