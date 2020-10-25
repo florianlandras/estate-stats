47de29a2ff5d9c114c4d1ed56dc53dcfa7c98d05
Morgan Pigeron <m.pigeron@hotmail.fr>
2020-10-25 21:15:28 +0100

    base rest consumer

--- frontend/estate-stats/src/app/tab1/tab1.page.ts
+++ frontend/estate-stats/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +8,38 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  public testVar = 'test var ';
+  private apiKey = 'https://api.nasa.gov/planetary/apod?api_key=CLHr8Zho80Tr2BcWGkU0w1opiWi01yEwhFRKWZWH'; //TODO never do that
+  
+  public apodImageUrl: string;
+  public apodExplanation: string;
+  public apodTitle: string;
+  public apodDate: string;
+  public apodCopy: string;
+
+  constructor(private http: HttpClient) {
+    this.getApodImageUrl();
+  }
+
+  /** Function to read API */
+  public readApi (url: string) {
+    return this.http.get(url);
+  }
+
+  /** function use to get url of picture of the day */
+  public getApodImageUrl () {
+    if (!this.apodImageUrl) { // if doesnt exist yet , get from NASA API
+      //read Api
+      this.readApi(this.apiKey)
+      .subscribe((data) => {
+        console.log(data);
+        this.apodImageUrl = data['url'];
+        this.apodExplanation = data['explanation'];
+        this.apodTitle = data['title'];
+        this.apodDate = data['date'];
+        this.apodCopy = data['copyright'];
+      });
+    }
+  }
 
 }
