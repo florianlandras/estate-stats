fda480edc20ba553fab0bf9fa152ae11bc76752d
Morgan Pigeron <m.pigeron@hotmail.fr>
2020-10-27 20:00:54 +0100

    hd sd switch

--- frontend/estate-stats/src/app/tab1/tab1.page.ts
+++ frontend/estate-stats/src/app/tab1/tab1.page.ts
@@ -11,11 +11,14 @@ export class Tab1Page {
   public testVar = 'test var ';
   private apiKey = 'https://api.nasa.gov/planetary/apod?api_key=CLHr8Zho80Tr2BcWGkU0w1opiWi01yEwhFRKWZWH'; //TODO never do that
   
+  public apodUrl: string;
   public apodImageUrl: string;
+  public apodImageHdUrl: string;
   public apodExplanation: string;
   public apodTitle: string;
   public apodDate: string;
-  public apodCopy: string;
+  public apodCopy: string; 
+  public apodSwitch: string = 'SD';
 
   constructor(private http: HttpClient) {
     this.getApodImageUrl();
@@ -34,11 +37,26 @@ export class Tab1Page {
       .subscribe((data) => {
         console.log(data);
         this.apodImageUrl = data['url'];
+        this.apodUrl = data['url']; // default value
         this.apodExplanation = data['explanation'];
         this.apodTitle = data['title'];
         this.apodDate = data['date'];
         this.apodCopy = data['copyright'];
+        this.apodImageHdUrl = data['hdurl'];
       });
+    }
+  }
+
+  /** function to switch between HD SD */
+  public hdSwitch (event) {
+    console.log(event);
+    if (event.detail.checked) { // on
+      this.apodSwitch = 'HD';
+      this.apodUrl = this.apodImageHdUrl;
+    }
+    else {
+      this.apodSwitch = 'SD';
+      this.apodUrl = this.apodImageUrl;    
     }
   }
 
